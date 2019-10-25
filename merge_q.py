58883e95418a83aad03e7ca736a2978c4a229bfa
nic <nwanavit@hatari.cc>
2019-10-25 22:57:02 +0200

    add merger randome selection

--- merge_q.py
+++ merge_q.py
@@ -15,8 +15,9 @@ def merge_dict_add_value(dict1, dict2):
 
 files = os.listdir('./q')
 files_without_main_q = [file for file in files if file not in ['q.json','q_updated.json']]
+if len(files_without_main_q) > 10:
+    files_without_main_q = np.array(files_without_main_q)[np.random.randint(0,len(files_without_main_q),10)]
 print(files_without_main_q)
-time.sleep(2)
 with open('./q/q.json', 'r') as f:
     q=json.load(f)
 
