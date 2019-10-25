19a7bd45241f7a19aaa69637cff0cfa8e0509424
nic <nwanavit@hatari.cc>
2019-10-25 12:18:26 +0200

    add inspect_q code

--- inspect_q.py
+++ inspect_q.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import json
-with open('./q.json','r') as f:
+with open('./q/q.json','r') as f:
     q=json.load(f)
 input_array = np.array([[j for j in i.values()] for i in q.values()]).flatten()
 out = np.concatenate(input_array).ravel()
